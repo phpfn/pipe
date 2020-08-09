435ea7610a59d7fc02b7db23d5368320b73afc5c
SerafimArts <nesk@xakep.ru>
2020-08-09 05:52:27 +0300

    Apply phpcs

--- src/polyfill.php
+++ src/polyfill.php
@@ -12,6 +12,9 @@ declare(strict_types=1);
 class_alias(\Fun\Pipe\Pipe::class, \Serafim\Pipe\Pipe::class);
 class_alias(\Fun\Pipe\PipeInterface::class, \Serafim\Pipe\PipeInterface::class);
 
-class_alias(\Fun\Pipe\Exception\FunctionNotFoundException::class, \Serafim\Pipe\Exception\FunctionNotFoundException::class);
+class_alias(
+    \Fun\Pipe\Exception\FunctionNotFoundException::class,
+    \Serafim\Pipe\Exception\FunctionNotFoundException::class
+);
 
 class_alias(\Fun\Pipe\Console\BuildAutocompleteCommand::class, \Serafim\Pipe\Console\BuildAutocompleteCommand::class);
